6cb7102119743a2b8958a5bfde6b004f6ba25a76
Dean <446578815@qq.com>
2018-07-19 09:29:26 +0800

    Add files via upload

---
+++ groupby.py
@@ -0,0 +1,85 @@
+# Define all the groupby transformations
+GROUPBY_AGGREGATIONS = [
+    
+    # V1 - GroupBy Features #
+    #########################    
+    # Variance in day, for ip-app-channel
+    {'groupby': ['ip','app','channel'], 'select': 'day', 'agg': 'var'},
+    # Variance in hour, for ip-app-os
+    {'groupby': ['ip','app','os'], 'select': 'hour', 'agg': 'var'},
+    # Variance in hour, for ip-day-channel
+    {'groupby': ['ip','day','channel'], 'select': 'hour', 'agg': 'var'},
+    # Count, for ip-day-hour
+    {'groupby': ['ip','day','hour'], 'select': 'channel', 'agg': 'count'},
+    # Count, for ip-app
+    {'groupby': ['ip', 'app'], 'select': 'channel', 'agg': 'count'},        
+    # Count, for ip-app-os
+    {'groupby': ['ip', 'app', 'os'], 'select': 'channel', 'agg': 'count'},
+    # Count, for ip-app-day-hour
+    {'groupby': ['ip','app','day','hour'], 'select': 'channel', 'agg': 'count'},
+    # Mean hour, for ip-app-channel
+    {'groupby': ['ip','app','channel'], 'select': 'hour', 'agg': 'mean'}, 
+    
+    # V2 - GroupBy Features #
+    #########################
+    # Average clicks on app by distinct users; is it an app they return to?
+    {'groupby': ['app'], 
+     'select': 'ip', 
+     'agg': lambda x: float(len(x)) / len(x.unique()), 
+     'agg_name': 'AvgViewPerDistinct'
+    },
+    # How popular is the app or channel?
+    {'groupby': ['app'], 'select': 'channel', 'agg': 'count'},
+    {'groupby': ['channel'], 'select': 'app', 'agg': 'count'},
+    
+    # V3 - GroupBy Features                                              #
+    # https://www.kaggle.com/bk0000/non-blending-lightgbm-model-lb-0-977 #
+    ###################################################################### 
+    {'groupby': ['ip'], 'select': 'channel', 'agg': 'nunique'}, 
+    {'groupby': ['ip'], 'select': 'app', 'agg': 'nunique'}, 
+    {'groupby': ['ip','day'], 'select': 'hour', 'agg': 'nunique'}, 
+    {'groupby': ['ip','app'], 'select': 'os', 'agg': 'nunique'}, 
+    {'groupby': ['ip'], 'select': 'device', 'agg': 'nunique'}, 
+    {'groupby': ['app'], 'select': 'channel', 'agg': 'nunique'}, 
+    {'groupby': ['ip', 'device', 'os'], 'select': 'app', 'agg': 'nunique'}, 
+    {'groupby': ['ip','device','os'], 'select': 'app', 'agg': 'cumcount'}, 
+    {'groupby': ['ip'], 'select': 'app', 'agg': 'cumcount'}, 
+    {'groupby': ['ip'], 'select': 'os', 'agg': 'cumcount'}, 
+    {'groupby': ['ip','day','channel'], 'select': 'hour', 'agg': 'var'}    
+]
+
+# Apply all the groupby transformations
+for spec in GROUPBY_AGGREGATIONS:
+    
+    # Name of the aggregation we're applying
+    agg_name = spec['agg_name'] if 'agg_name' in spec else spec['agg']
+    
+    # Name of new feature
+    new_feature = '{}_{}_{}'.format('_'.join(spec['groupby']), agg_name, spec['select'])
+    
+    # Info
+    print("Grouping by {}, and aggregating {} with {}".format(
+        spec['groupby'], spec['select'], agg_name
+    ))
+    
+    # Unique list of features to select
+    all_features = list(set(spec['groupby'] + [spec['select']]))
+    
+    # Perform the groupby
+    gp = X_train[all_features]. \
+        groupby(spec['groupby'])[spec['select']]. \
+        agg(spec['agg']). \
+        reset_index(). \
+        rename(index=str, columns={spec['select']: new_feature})
+        
+    # Merge back to X_total
+    if 'cumcount' == spec['agg']:
+        X_train[new_feature] = gp[0].values
+    else:
+        X_train = X_train.merge(gp, on=spec['groupby'], how='left')
+        
+     # Clear memory
+    del gp
+    gc.collect()
+
+X_train.head()
---
+++ missing_value.py
@@ -0,0 +1,30 @@
+# Function to calculate missing values by column# Funct 
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+# Missing values statistics
+missing_values = missing_values_table(app_train)
+missing_values.head(20)
