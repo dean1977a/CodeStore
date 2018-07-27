df4b512d36872f6fad3cd32aa159b69798b7c5a4
Dean <446578815@qq.com>
2018-07-27 18:21:25 +0800

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -17,6 +17,32 @@ for i, source in enumerate(['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']):
     
 plt.tight_layout(h_pad = 2.5)
 ########################################################################################################
+# Plots the disribution of a variable colored by value of the target
+def kde_target(var_name, df):
+    
+    # Calculate the correlation coefficient between the new variable and the target
+    corr = df['TARGET'].corr(df[var_name])
+    
+    # Calculate medians for repaid vs not repaid
+    avg_repaid = df.ix[df['TARGET'] == 0, var_name].median()
+    avg_not_repaid = df.ix[df['TARGET'] == 1, var_name].median()
+    
+    plt.figure(figsize = (12, 6))
+    
+    # Plot the distribution for target == 0 and target == 1
+    sns.kdeplot(df.ix[df['TARGET'] == 0, var_name], label = 'TARGET == 0')
+    sns.kdeplot(df.ix[df['TARGET'] == 1, var_name], label = 'TARGET == 1')
+    
+    # label the plot
+    plt.xlabel(var_name); plt.ylabel('Density'); plt.title('%s Distribution' % var_name)
+    plt.legend();
+    
+    # print out the correlation
+    print('The correlation between %s and the TARGET is %0.4f' % (var_name, corr))
+    # Print out average values
+    print('Median value for loan that was not repaid = %0.4f' % avg_not_repaid)
+    print('Median value for loan that was repaid =     %0.4f' % avg_repaid)
+###########################################################################################################
 
 # kde分布
 f,ax = plt.subplots(figsize=(10,5))
