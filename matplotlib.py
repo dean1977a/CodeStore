9bac1a09d2f3df8b5d5f2ca5da3ff036a51ec592
Dean <446578815@qq.com>
2018-08-22 09:57:39 +0800

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -217,20 +217,4 @@ def plot_numerical(data, col, size=[8, 4], bins=50):
 plot_numerical(application_train, 'AMT_CREDIT')
 
 #############################################################################################################
-#split categorical, discrete and numerical features  切分类别变量和数值变量
-def feature_type_split(data, special_list=[]):
-    cat_list = []
-    dis_num_list = []
-    num_list = []
-    for i in data.columns.tolist():
-        if data[i].dtype == 'object':
-            cat_list.append(i)
-        elif data[i].nunique() < 25:
-            dis_num_list.append(i)
-        elif i in special_list:     # if you want to add some special cases
-            dis_num_list.append(i)
-        else:
-            num_list.append(i)
-    return cat_list, dis_num_list, num_list
-#示例
-cat_list, dis_num_list, num_list = feature_type_split(application_train, special_list=['AMT_REQ_CREDIT_BUREAU_YEAR']) 
+
