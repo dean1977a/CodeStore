4e83b6b19554298da79e8a2a74d008e0a47989bd
Dean <446578815@qq.com>
2018-08-30 22:07:56 +0800

    Update Automated Model Tuning.py

--- Python/Automated Model Tuning.py
+++ Python/Automated Model Tuning.py
@@ -1,5 +1,7 @@
 # coding: utf-8
+#两种自动调参代码，第一种是单lgb模型调参，第二种是多种模型同时调参
 
+#Type ONE
 
 # Data manipulation
 import pandas as pd
@@ -180,3 +182,72 @@ print(trials_dict[:1])
 with open('trials.json', 'w') as f:
     f.write(json.dumps(trials_dict))
 
+#Type Two
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target
+print X.shape, y.shape
+
+def hyperopt_train_test(params):
+    t = params['type']
+    del params['type']
+    if t == 'naive_bayes':
+        clf = BernoulliNB(**params)
+    elif t == 'svm':
+        clf = SVC(**params)
+    elif t == 'dtree':
+        clf = DecisionTreeClassifier(**params)
+    elif t == 'knn':
+        clf = KNeighborsClassifier(**params)
+    else:
+        return 0
+    return cross_val_score(clf, X, y).mean()
+
+space = hp.choice('classifier_type', [
+    {
+        'type': 'naive_bayes',
+        'alpha': hp.uniform('alpha', 0.0, 2.0)
+    },
+    {
+        'type': 'svm',
+        'C': hp.uniform('C', 0, 10.0),
+        'kernel': hp.choice('kernel', ['linear', 'rbf']),
+        'gamma': hp.uniform('gamma', 0, 20.0)
+    },
+    {
+        'type': 'randomforest',
+        'max_depth': hp.choice('max_depth', range(1,20)),
+        'max_features': hp.choice('max_features', range(1,5)),
+        'n_estimators': hp.choice('n_estimators', range(1,20)),
+        'criterion': hp.choice('criterion', ["gini", "entropy"]),
+        'scale': hp.choice('scale', [0, 1]),
+        'normalize': hp.choice('normalize', [0, 1])
+    },
+    {
+        'type': 'knn',
+        'n_neighbors': hp.choice('knn_n_neighbors', range(1,50))
+    }
+])
+
+count = 0
+best = 0
+def f(params):
+    global best, count
+    count += 1
+    acc = hyperopt_train_test(params.copy())
+    if acc > best:
+        print 'new best:', acc, 'using', params['type']
+        best = acc
+    if count % 50 == 0:
+        print 'iters:', count, ', acc:', acc, 'using', params
+    return {'loss': -acc, 'status': STATUS_OK}
+
+trials = Trials()
+best = fmin(f, space, algo=tpe.suggest, max_evals=1500, trials=trials)
+print 'best:'
+print best
+
+作者：ktulu7
+链接：https://www.jianshu.com/p/35eed1567463
+來源：简书
+简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
