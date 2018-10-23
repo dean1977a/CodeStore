575a273b07289c6a44b120062c4ad55916eeca50
Dean <446578815@qq.com>
2018-10-24 06:48:33 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -200,6 +200,51 @@ plt.show()
 data_train = model.fit_transform(data_train)
 data_test = model.fit_transform(data_test)
 
+# 11. 绘制学习曲线，以确定模型的状况是否过拟合和欠拟合
+def plot_learning_curve(estimator,title, X, y,ylim=(0.8, 1.01), cv=None,
+                        train_sizes=np.linspace(.05, 0.2, 5)):
+    """
+    画出data在某模型上的learning curve.
+    参数解释
+    ----------
+    estimator : 你用的分类器。
+    title : 表格的标题。
+    X : 输入的feature，numpy类型
+    y : 输入的target vector
+    ylim : tuple格式的(ymin, ymax), 设定图像中纵坐标的最低点和最高点
+    cv : 做cross-validation的时候，数据分成的份数，其中一份作为cv集，其余n-1份作为training(默认为3份)
+    """
+    plt.figure()
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=5, n_jobs=1, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.legend(loc="best")
+    plt.grid("on")
+    if ylim:
+        plt.ylim(ylim)
+    plt.title(title)
+    plt.show()
+
+--------------------- 
+作者：hhy518518 
+来源：CSDN 
+原文：https://blog.csdn.net/hhy518518/article/details/54847688 
+版权声明：本文为博主原创文章，转载请附上博文链接！
+
 # 创建模型
 # 1. import xgboost as xgb
 from sklearn.cross_validation import KFold, cross_val_score
