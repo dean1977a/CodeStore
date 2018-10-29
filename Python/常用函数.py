8929b9953e3af931ebdeb3c74d2c913353f34b1c
Dean <446578815@qq.com>
2018-10-30 07:21:18 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -103,6 +103,42 @@ first = dat_edge_1_weight['first'].str.split(':',expand=True,)
 #如果数据中含有缺失值、特殊字符astype()函数可能失效。
 df['Date of Publication'] = pd.to_numeric(extr)
 
+
+
+# 12.1 文本处理 
+# convert to lower case  转小写
+suicide_attacks['City'] = suicide_attacks['City'].str.lower()
+# remove trailing white spaces   去空格
+suicide_attacks['City'] = suicide_attacks['City'].str.strip()
+
+# 12.2 文本清洗，剔除空格，匹配接近的单词
+# 包依赖： fuzzywuzzy 
+# function to replace rows in the provided column of the provided dataframe
+# that match the provided string above the provided ratio with the provided string
+def replace_matches_in_column(df, column, string_to_match, min_ratio = 90):
+    # get a list of unique strings
+    strings = df[column].unique()
+    
+    # get the top 10 closest matches to our input string
+    matches = fuzzywuzzy.process.extract(string_to_match, strings, 
+                                         limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+
+    # only get matches with a ratio > 90
+    close_matches = [matches[0] for matches in matches if matches[1] >= min_ratio]
+
+    # get the rows of all the close matches in our dataframe
+    rows_with_matches = df[column].isin(close_matches)
+
+    # replace all rows with close matches with the input matches 
+    df.loc[rows_with_matches, column] = string_to_match
+    
+    # let us know the function's done
+    print("All done!")
+#应用示例
+replace_matches_in_column(df=suicide_attacks, column='City', string_to_match="d.i khan")
+
+
+
 # 12. 用随机森林处理年龄缺失示例
 def setMissingData(df,features=[],missFeature='Age'):
     feature_df = df[features]
@@ -161,31 +197,7 @@ def processAge(df):
     # df['Age_bin_id_scaled'] = scaler.fit_transform(df['Age_bin_id'])
     return  df
 
-# 13 文本清洗，剔除空格，匹配接近的单词
-# 包依赖： fuzzywuzzy 
-# function to replace rows in the provided column of the provided dataframe
-# that match the provided string above the provided ratio with the provided string
-def replace_matches_in_column(df, column, string_to_match, min_ratio = 90):
-    # get a list of unique strings
-    strings = df[column].unique()
-    
-    # get the top 10 closest matches to our input string
-    matches = fuzzywuzzy.process.extract(string_to_match, strings, 
-                                         limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio)
 
-    # only get matches with a ratio > 90
-    close_matches = [matches[0] for matches in matches if matches[1] >= min_ratio]
-
-    # get the rows of all the close matches in our dataframe
-    rows_with_matches = df[column].isin(close_matches)
-
-    # replace all rows with close matches with the input matches 
-    df.loc[rows_with_matches, column] = string_to_match
-    
-    # let us know the function's done
-    print("All done!")
-#应用示例
-replace_matches_in_column(df=suicide_attacks, column='City', string_to_match="d.i khan")
 
 
 
