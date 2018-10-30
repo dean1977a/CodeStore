a0b540759a29440a6c6864c6ce1f0d2f0be032d9
Dean <446578815@qq.com>
2018-10-31 06:15:47 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -138,8 +138,44 @@ def replace_matches_in_column(df, column, string_to_match, min_ratio = 90):
 replace_matches_in_column(df=suicide_attacks, column='City', string_to_match="d.i khan")
 
 
+# 12. 计算两个日期之间的时间差
+def dataInterval(data1,data2):
+    d1 = datetime.datetime.strptime(data1, '%Y-%m-%d')
+    d2 = datetime.datetime.strptime(data2, '%Y-%m-%d')
+    delta = d1 - d2
+    return delta.days
 
-# 12. 用随机森林处理年龄缺失示例
+def getInterval(arrLike):  #用来计算日期间隔天数的调用的函数
+    PublishedTime = arrLike['PublishedTime']
+    ReceivedTime = arrLike['ReceivedTime']
+#    print(PublishedTime.strip(),ReceivedTime.strip())
+    days = dataInterval(PublishedTime.strip(),ReceivedTime.strip())  #注意去掉两端空白
+    return days
+
+def getInterval_new(arrLike,before,after):  #用来计算日期间隔天数的调用的函数
+    before = arrLike[before]
+    after = arrLike[after]
+#    print(PublishedTime.strip(),ReceivedTime.strip())
+    days = dataInterval(after.strip(),before.strip())  #注意去掉两端空白
+    return days
+
+
+if __name__ == '__main__':    
+    fileName = "NS_new.xls";
+    df = pd.read_excel(fileName) 
+    df['TimeInterval'] = df.apply(getInterval , axis = 1)
+    df['TimeInterval'] = df.apply(getInterval_new , 
+      axis = 1, args = ('ReceivedTime','PublishedTime'))    #调用方式一
+    #下面的调用方式等价于上面的调用方式
+    df['TimeInterval'] = df.apply(getInterval_new , 
+      axis = 1, **{'before':'ReceivedTime','after':'PublishedTime'})  #调用方式二
+    #下面的调用方式等价于上面的调用方式
+    df['TimeInterval'] = df.apply(getInterval_new , 
+      axis = 1, before='ReceivedTime',after='PublishedTime')  #调用方式三
+
+
+
+# 13. 用随机森林处理年龄缺失示例
 def setMissingData(df,features=[],missFeature='Age'):
     feature_df = df[features]
     X = feature_df[df[missFeature].notnull()].as_matrix()[:,1::]
