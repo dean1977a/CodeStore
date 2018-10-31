166f36cca9dfee749367d02307a9dd95e0111b8b
Dean <446578815@qq.com>
2018-11-01 06:53:15 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -352,6 +352,17 @@ dummies = pd.get_dummies(data=data_all[ob], prefix="{}#".format(ob))
 data_all.drop(ob, axis=1, inplace=True)
 data_all = data_all.join(dummies)
 
+
+# 7.1 
+def one_hot_encoder(df, nan_as_category = True):
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
+
+
+
 # 8. 用radio中位数填补空缺
 kitch_ratio = train_df['full_sq']/train_df['kitch_sq']
 train_df['kitch_sq']=train_df['kitch_sq'].fillna(train_df['full_sq'] /kitch_ratio.median())
