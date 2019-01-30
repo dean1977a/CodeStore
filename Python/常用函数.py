ba4de7dfcce94a1d88d3b404cf1c7a506746c683
Dean <446578815@qq.com>
2019-01-30 14:17:51 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -406,6 +406,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from IPython.display import display
 %matplotlib inline
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
 import plotly.offline as py
 import plotly.graph_objs as go
