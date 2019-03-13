2ff0585530742b92cf588977826984c0a23ac38e
Dean <446578815@qq.com>
2019-03-13 17:38:17 +0800

    Update split categorical, discrete and numerical features.py

--- split categorical, discrete and numerical features.py
+++ split categorical, discrete and numerical features.py
@@ -1,17 +1,20 @@
 #split categorical, discrete and numerical features  切分类别变量和数值变量
-def feature_type_split(data, special_list=[]):
+#通过手工添加special_num_list,special_cat_list不断排除异常变量
+def feature_type_split(data,special_num_list=[],special_cat_list=[]):
     cat_list = []
     dis_num_list = []
     num_list = []
     for i in data.columns.tolist():
         if data[i].dtype == 'object':
             cat_list.append(i)
-        elif data[i].nunique() < 25:
-            dis_num_list.append(i)
-        elif i in special_list:     # if you want to add some special cases
+        elif i in special_cat_list:     # if you want to add some special cases
+            cat_list.append(i)
+        elif i in special_num_list:     # if you want to add some special cases
+            num_list.append(i)
+        elif data[i].nunique() < 10:
             dis_num_list.append(i)
         else:
             num_list.append(i)
     return cat_list, dis_num_list, num_list
 #示例
-cat_list, dis_num_list, num_list = feature_type_split(application_train, special_list=['AMT_REQ_CREDIT_BUREAU_YEAR']) 
+categorical_var, dis_num_list, numerical_var = feature_type_split(trainData, special_num_list=[],special_cat_list=[]) 
