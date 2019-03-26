6f049bd3028a6c582ce3df07a3e5cf887e40b564
Dean <446578815@qq.com>
2019-03-26 16:56:16 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -1,3 +1,128 @@
+from plotly.offline import init_notebook_mode, iplot
+from wordcloud import WordCloud
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import plotly.plotly as py
+from plotly import tools
+from datetime import date
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import random 
+import warnings
+warnings.filterwarnings("ignore")
+init_notebook_mode(connected=True)
+
+path = "../input/"
+
+def bar_hor(df, col, title, color, w=None, h=None, lm=0, limit=100, return_trace=False, rev=False, xlb = False):
+    """
+    横向き棒グラフ作成関数
+    df:  
+    col: 
+    title: 
+    color: 
+    w=None: 
+    h=None: 
+    lm=0: 
+    limit=100: 
+    return_trace=False: 
+    rev=False: 
+    xlb = False:
+    """
+    cnt_srs = df[col].value_counts()
+    yy = cnt_srs.head(limit).index[::-1] 
+    xx = cnt_srs.head(limit).values[::-1] 
+    if rev:
+        yy = cnt_srs.tail(limit).index[::-1] 
+        xx = cnt_srs.tail(limit).values[::-1] 
+    if xlb:
+        trace = go.Bar(y=xlb, x=xx, orientation = 'h', marker=dict(color=color))
+    else:
+        trace = go.Bar(y=yy, x=xx, orientation = 'h', marker=dict(color=color))
+    if return_trace:
+        return trace 
+    layout = dict(title=title, margin=dict(l=lm), width=w, height=h)
+    data = [trace]
+    fig = go.Figure(data=data, layout=layout)
+    iplot(fig)
+
+def bar_hor_noagg(x, y, title, color, w=None, h=None, lm=0, limit=100, rt=False):
+    trace = go.Bar(y=x, x=y, orientation = 'h', marker=dict(color=color))
+    if rt:
+        return trace
+    layout = dict(title=title, margin=dict(l=lm), width=w, height=h)
+    data = [trace]
+    fig = go.Figure(data=data, layout=layout)
+    iplot(fig)
+
+
+def bar_ver_noagg(x, y, title, color, w=None, h=None, lm=0, rt = False):
+    trace = go.Bar(y=y, x=x, marker=dict(color=color))
+    if rt:
+        return trace
+    layout = dict(title=title, margin=dict(l=lm), width=w, height=h)
+    data = [trace]
+    fig = go.Figure(data=data, layout=layout)
+    iplot(fig)
+    
+def gp(col, title):
+    """
+    グループ化棒グラフを表示
+    col: 表示する列
+    title: 図のタイトル
+    """
+    df1 = app_train[app_train["TARGET"] == 1]
+    df0 = app_train[app_train["TARGET"] == 0]
+    a1 = df1[col].value_counts()
+    b1 = df0[col].value_counts()
+
+    trace1 = go.Bar(x=a1.index, y=a1.values, name='Target : 1', marker=dict(color="#44ff54"))
+    trace2 = go.Bar(x=b1.index, y=b1.values, name='Target : 0', marker=dict(color="#ff4444"))
+
+    data = [trace1, trace2]
+    layout = go.Layout(barmode='group', height=300, title = title)
+
+    fig = go.Figure(data=data, layout=layout)
+    iplot(fig, filename='grouped-bar')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 数据预处理
 # 1. 读取数据：
 data_macro = pd.read_csv("macro.csv", parse_dates=['timestamp'], usecols=['timestamp'] + macro_cols)
