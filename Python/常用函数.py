35d1cff3b32802f12daf9154186138a5469d6ec8
Dean <446578815@qq.com>
2019-03-26 17:04:40 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -1,18 +1,42 @@
+from sklearn.metrics import roc_auc_score, roc_curve, precision_score, recall_score
+from sklearn.model_selection import KFold, StratifiedKFold, train_test_split
 from plotly.offline import init_notebook_mode, iplot
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.preprocessing import scale
+from lightgbm import LGBMClassifier
+from sklearn.svm import SVC
 from wordcloud import WordCloud
+import statsmodels.api as sm
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 import plotly.plotly as py
 from plotly import tools
 from datetime import date
+import datetime, time
+from pylab import mpl
+import seaborn as sns
 import pandas as pd
 import numpy as np 
-import seaborn as sns
 import random 
 import warnings
+import random
+import pickle
+import sys
+import gc
+
+
+folderOfData = 'C:\\Users\\Dean\\Desktop\\model'
 warnings.filterwarnings("ignore")
 init_notebook_mode(connected=True)
-
+pd.set_option('max_columns',300) 
+pd.set_option('max_row',300)
+sys.path.append(folderOfData)
 path = "../input/"
 
 def bar_hor(df, col, title, color, w=None, h=None, lm=0, limit=100, return_trace=False, rev=False, xlb = False):
@@ -85,7 +109,55 @@ def gp(col, title):
 
     fig = go.Figure(data=data, layout=layout)
     iplot(fig, filename='grouped-bar')
-
+# Function to calculate missing values by column# Funct 
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+# Missing values statistics
+# Find one unqiue value
+def find_one_unqiue_values(df):
+# We will takeoff all columns where we have a unique value (constants)
+# It is useful because this columns don't give us none information
+   discovering_consts = [col for col in df.columns if df[col].nunique() == 1]
+   return discovering_consts
+# printing the total of columns dropped and the name of columns 
+   print("Columns with just one value: ", len(discovering_consts), "columns")
+   print("Name of constant columns: \n", discovering_consts)
+# 15.1 显示每个特征的唯一值
+# seting the function to show 
+def knowningData(df, data_type=object, limit=3): #seting the function with df, 
+    n = df.select_dtypes(include=data_type) #selecting the desired data type
+    for column in n.columns: #initializing the loop
+        print("##############################################")
+        print("Name of column ", column, ': \n', "Uniques: ", df[column].unique()[:limit], "\n",
+              " | ## Total nulls: ", (round(df[column].isnull().sum() / len(df[column]) * 100,2)),
+              " | ## Total unique values: ", df.nunique()[column]) #print the data and % of nulls)
+        # print("Percentual of top 3 of: ", column)
+        # print(round(df[column].value_counts()[:3] / df[column].value_counts().sum() * 100,2))
+print("#############################################")
 
 
 
