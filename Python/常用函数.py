427ae5bd12a1601b0b48c0e120d24ba2039cbaa0
Dean <446578815@qq.com>
2019-12-30 22:12:54 +0800

    Update 常用函数.py

--- Python/常用函数.py
+++ Python/常用函数.py
@@ -74,90 +74,6 @@ def plotbar(df,var,target):
 #示例
 plotbar(df,'gender,'target')
 
-def bar_hor(df, col, title, color, w=None, h=None, lm=0, limit=100, return_trace=False, rev=False, xlb = False):
-    """
-    横向き棒グラフ作成関数
-    df:  
-    col: 
-    title: 
-    color: 
-    w=None: 
-    h=None: 
-    lm=0: 
-    limit=100: 
-    return_trace=False: 
-    rev=False: 
-    xlb = False:
-    """
-    cnt_srs = df[col].value_counts()
-    yy = cnt_srs.head(limit).index[::-1] 
-    xx = cnt_srs.head(limit).values[::-1] 
-    if rev:
-        yy = cnt_srs.tail(limit).index[::-1] 
-        xx = cnt_srs.tail(limit).values[::-1] 
-    if xlb:
-        trace = go.Bar(y=xlb, x=xx, orientation = 'h', marker=dict(color=color))
-    else:
-        trace = go.Bar(y=yy, x=xx, orientation = 'h', marker=dict(color=color))
-    if return_trace:
-        return trace 
-    layout = dict(title=title, margin=dict(l=lm), width=w, height=h)
-    data = [trace]
-    fig = go.Figure(data=data, layout=layout)
-    iplot(fig)
-
-def bar_hor_noagg(x, y, title, color, w=None, h=None, lm=0, limit=100, rt=False):
-    trace = go.Bar(y=x, x=y, orientation = 'h', marker=dict(color=color))
-    if rt:
-        return trace
-    layout = dict(title=title, margin=dict(l=lm), width=w, height=h)
-    data = [trace]
-    fig = go.Figure(data=data, layout=layout)
-    iplot(fig)
-
-
-def bar_ver_noagg(x, y, title, color, w=None, h=None, lm=0, rt = False):
-    trace = go.Bar(y=y, x=x, marker=dict(color=color))
-    if rt:
-        return trace
-    layout = dict(title=title, margin=dict(l=lm), width=w, height=h)
-    data = [trace]
-    fig = go.Figure(data=data, layout=layout)
-    iplot(fig)
-    
-def gp(col, title):
-    """
-    グループ化棒グラフを表示
-    col: 表示する列
-    title: 図のタイトル
-    """
-    df1 = app_train[app_train["TARGET"] == 1]
-    df0 = app_train[app_train["TARGET"] == 0]
-    a1 = df1[col].value_counts()
-    b1 = df0[col].value_counts()
-
-    trace1 = go.Bar(x=a1.index, y=a1.values, name='Target : 1', marker=dict(color="#44ff54"))
-    trace2 = go.Bar(x=b1.index, y=b1.values, name='Target : 0', marker=dict(color="#ff4444"))
-
-    data = [trace1, trace2]
-    layout = go.Layout(barmode='group', height=300, title = title)
-
-    fig = go.Figure(data=data, layout=layout)
-    iplot(fig, filename='grouped-bar')
-    
-def exploreCat(col):
-    t = application[col].value_counts()
-    labels = t.index
-    values = t.values
-    colors = ['#96D38C','#FEBFB3']
-    trace = go.Pie(labels=labels, values=values,
-                   hoverinfo="all", textinfo='value',
-                   textfont=dict(size=12),
-                   marker=dict(colors=colors,
-                               line=dict(color='#fff', width=2)))
-    layout = go.Layout(title=col, height=400)
-    fig = go.Figure(data=[trace], layout=layout)
-    iplot(fig)
 
 #the realation between the categorical column and the target 
 def catAndTrgt(col):
@@ -208,6 +124,7 @@ def find_one_unqiue_values(df):
 # printing the total of columns dropped and the name of columns 
    print("Columns with just one value: ", len(discovering_consts), "columns")
    print("Name of constant columns: \n", discovering_consts)
+   
 # 15.1 显示每个特征的唯一值
 # seting the function to show 
 def knowningData(df, data_type=object, limit=3): #seting the function with df, 
